6b72be9bb1c5c85d7e89613f61e7ffe41ff6fcf2
LokiiiGo <lucascalle880@gmail.com>
2025-12-06 22:13:47 -0300

    Adicionando descrições

--- data/posts.php
+++ data/posts.php
@@ -4,7 +4,7 @@ $posts = [
         'id'          => 1,
         'title'       => 'Programação PHP',
         'description' => 'PHP é uma linguagem de script do lado do servidor, o que significa que o código PHP é executado no servidor web antes que a página seja enviada para o navegador do usuário.
-        Isso permite que o PHP gere conteúdo dinâmico, interaja com bancos de dados e processe informações antes de exibir a página no navegador. 
+        Isso permite que o PHP gere conteúdo dinâmico, interaja com bancos de dados e processe informações antes de exibir a página no navegador.
         <br><br>
         PRINCIPAIS CARACTERISTICAS SOBRE PHP
         <br><br>
@@ -69,13 +69,13 @@ $posts = [
         'img'         => 'portugolIDE1.png',
     ],
     [
-        'id' => 6,
-        'title' => 'MySQL - Banco de Dados Relacional',
+        'id'          => 6,
+        'title'       => 'MySQL - Banco de Dados Relacional',
         'description' => 'É um sistema de gerenciador de bancode dados relacional de código aberto usado na maioria das aplicações gratuitas
         para gerir suas bases de dados. O serviço utiliza a linguagem SQL (Structure Query Language) traduzido para Lingaugem de Consulta Estruturada, que
         é uma linguagem mais popular pra inserir, acessar e gerenciar o conteúdo armazenado num banco de dados.
         <br><br>
-        O MySQL é usado em uma ampla variedade de aplicações como: 
+        O MySQL é usado em uma ampla variedade de aplicações como:
         <br><br>
         - Sites de comércios eletrônicos.
         <br><br>
@@ -89,7 +89,53 @@ $posts = [
         <br><br>
         Funciona em um modelo cliente-servidor, onde os dados ficam armazenados no servidor e os clientes fazem solicitações para acessá-los.
         Os clientes podem fazer solicitações atravéz de uma Interface Gráfica do Usuário (IGU) em suas telas.',
-        'tags' => ['Banco de Dados', 'Mysql', 'SQL'],
-        'img' => 'mysql.png',
+        'tags'        => ['Banco de Dados', 'Mysql', 'SQL'],
+        'img'         => 'mysql.png',
+    ],
+    [
+        'id'          => 7,
+        'title'       => 'O que é XAMPP',
+        'description' => 'É uma distribuição de software gratuito e de código aberto que contém o Apache, MySQL, PHP e Perl. A sigla XAMPP é uma referencias a esses quatro ferrasmentas, sendo que o X inicial indica que uma ferramenta é multiplataforma,
+        ou seja, funciona em Windows, Linux e MAC.
+        <br>
+        Foi desenvolvido pela Apache Friends, uma organização sem fins lucrativos , é considierado o ambiente de desenvolvimento PHP mais popular.
+        Foi criado para ser uma ferramenta de desenvolvimento que permitisse ao programadores de websites testar o seu trabalho sem a necessidade de acesso a internet. No entanto, atualmente,
+        o XAMPP é também utilizado para servir websites WWW.
+        <br><br>
+        - Possui um painel de controle que permite iniciar ou parar servidores individuais.',
+        'tags'        => ['Software', 'Apavhe Friends'],
+        'img'         => 'xampp.png',
+    ],
+    [
+        'id' => 8,
+        'title' => 'Visual Studio Code',
+        'description' => 'O VSCode é um editor de código-fonte gratuito, de código aberto e multiplataforma desenvolvido pela Microsoft.
+        <br><br>
+        - É compatível com Windows, Linux e MAC.
+        <br><br>
+        - Suporta várias linguagens como JavaScript, PHP, C, C++, JAVA, TypeScript, Python, etc.
+        <br><br>
+        - É personalizável, permitindo alterar a aparência, teclas de atalho e preferências.
+        <br><br>
+        - É uma ferramenta de código-aberto, o que permite que a comunidade a melhore constantemente.
+        <br><br>
+        O VSCode é uma das ferramentas mais populares entre programadores e desenvolvedores. A sua popularidade deve-se a vários fatores, como a possibilidade 
+        de personalização e o fato de ser gratuito.
+        <br><br>
+        <a href="https://code.visualstudio.com/">Para instalação do VSCode, clique aqui</a>',
+        'tags' => ['Multiplataforma', 'Ferramenta'],
+        'img' => 'vscode.png',
+    ],
+    [
+        'id'=> 9,
+        'title' => 'GitHub - Rede social de Programador',
+        'description' => 'Uma plataforma de desenvolvimento colaborativo que aloja projetor na nuvem utilizando o sistemas de controle de versões chamado <strong>Git</strong>. 
+        O github ajuda os desenvolvedores a armazenar e administrar o código e fazer o registro de mudança.
+        Geralmente, é de código-aberto, o que permite realizar projetos compartilhados e manter o acompanhamento detalhado do usuário.
+        <br>
+        Também funciona como uma rede social, conectando os desenvolvedores com os usuários. Como usuário, podem descarregar programas ou aplicativos, e da mesma maneira, colaborar com o 
+        desenvolvimento oferecendo melhorias e discutindo as questões que interessam nos foruns temáticos.',
+        'tags' => ['Git', 'Rede Social', 'Plataforma'],
+        'img' => 'git-github.jpg',
     ],
 ];
